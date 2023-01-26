fc6161f532922326e2033e047f40d05c5d16b428
Aldo Schumann <git@aldo.io>
2023-01-27 03:37:06 +1100

    wip: staff admin ui

---
+++ packages/bookings/staff-app/src/app/App.tsx
@@ -0,0 +1,234 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
+
+export function App() {
+  return (
+    <div className="container m-auto p-4 pr-0 pt-8">
+      <div className="navbar bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-200 text-base-content shadow-sm rounded-xl shadow mb-4 gap-4">
+        <div className="flex-one">
+          <a className="btn btn-ghost normal-case text-xl gap-2 rounded-xl">
+            <svg
+              className="w-6 h-6"
+              viewBox="0 0 114 109"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                width="72.4437"
+                height="72.4437"
+                rx="4"
+                transform="matrix(0.780862 -0.624704 0.780862 0.624704 0 45.2559)"
+                fill="#19D893"
+              />
+              <rect
+                width="72.4437"
+                height="72.4437"
+                rx="4"
+                transform="matrix(0.780862 -0.624704 0.780862 0.624704 0 63.2561)"
+                fill="#19D893"
+                fill-opacity="0.4"
+              />
+            </svg>
+            Bookings
+          </a>
+        </div>
+        <div className="basis-1/5"></div>
+        <div className="flex-1">
+          <div className="form-control w-full min-w-96">
+            <input type="text" placeholder="Search" className="input w-full" />
+          </div>
+        </div>
+        <div className="basis-1/5"></div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </label>
+            <div
+              tabIndex={0}
+              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-12 gap-4 h-80">
+        <div className="col-span-3">
+          <div className="card bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-200 p-4 h-full">
+            <a className="btn btn-block btn-ghost normal-case gap-2 rounded-xl p-2 text-left justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97-1.94.284-3.916.455-5.922.505a.39.39 0 00-.266.112L8.78 21.53A.75.75 0 017.5 21v-3.955a48.842 48.842 0 01-2.652-.316c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Messages
+              <div className="flex-auto"></div>
+              <div className="flex-initial">
+                <span className="indicator-item badge badge-success text-xs">
+                  99+
+                </span>
+              </div>
+            </a>
+
+            <a className="btn btn-block btn-ghost normal-case gap-2 rounded-xl p-2 text-left justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path d="M5.566 4.657A4.505 4.505 0 016.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0015.75 3h-7.5a3 3 0 00-2.684 1.657zM2.25 12a3 3 0 013-3h13.5a3 3 0 013 3v6a3 3 0 01-3 3H5.25a3 3 0 01-3-3v-6zM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 016.75 6h10.5a3 3 0 012.683 1.657A4.505 4.505 0 0018.75 7.5H5.25z" />
+              </svg>
+              Reminders
+            </a>
+            <a className="btn btn-block btn-ghost normal-case gap-2 rounded-xl p-2 text-left justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Calendar
+            </a>
+            <div className="divider"></div>
+          </div>
+        </div>
+        <div className="col-span-9 p-4 rounded-l-none">content</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+if (import.meta.vitest) {
+  // add tests related to your file here
+  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
+
+  const { it, expect, beforeEach } = import.meta.vitest;
+  let render: any;
+
+  beforeEach(async () => {
+    render = (await import('@testing-library/react')).render;
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+
+        {/* START: routes */}
+        {/* These routes and navigation have been generated for you */}
+        {/* Feel free to move and update them to fit your needs */}
+        <br />
+        <hr />
+        <br />
+        <div role="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page-2">Page 2</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                This is the generated root route.{' '}
+                <Link to="/page-2">Click here for page 2.</Link>
+              </div>
+            }
+          />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+        {/* END: routes */}
+      </BrowserRouter>,
+    );
+    expect(getByText(/Welcome bookings-staff-app/gi)).toBeTruthy();
+  });
+}
